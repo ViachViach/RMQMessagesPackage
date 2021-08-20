450caabe52cf1b6b86aade9f98d4a72bd2f90cc3
Karmazin <vyacheslavkarmazin@gmail.com>
2021-08-20 16:47:42 +0200

    - add new messages

---
+++ src/ChangePasswordMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RMQ\Messages;
+
+class ChangePasswordMessage
+{
+    private string $login;
+    private string $password;
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): ChangePasswordMessage
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): ChangePasswordMessage
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
---
+++ src/CheckingAccountVvoB1Message.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RMQ\Messages;
+
+class CheckingAccountVvoB1Message extends AccountMessage
+{
+}
