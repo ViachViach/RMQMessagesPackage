3073ca289dcd7b41224b5d71d988d1fdc5c65651
Karmazin <vyacheslavkarmazin@gmail.com>
2021-08-22 01:12:05 +0200

    - add new messages

--- src/AppointmentAccountMessage.php
+++ src/AppointmentAccountMessage.php
@@ -16,4 +16,54 @@ class AppointmentAccountMessage
     */
     private array $dateInterval;
     private int $waitDate;
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): AppointmentAccountMessage
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): AppointmentAccountMessage
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return DateTimeImmutable[]
+     */
+    public function getDateInterval(): array
+    {
+        return $this->dateInterval;
+    }
+
+    /**
+     * @param DateTimeImmutable[] $dateInterval
+     */
+    public function setDateInterval(array $dateInterval): AppointmentAccountMessage
+    {
+        $this->dateInterval = $dateInterval;
+        return $this;
+    }
+
+    public function getWaitDate(): int
+    {
+        return $this->waitDate;
+    }
+
+    public function setWaitDate(int $waitDate): AppointmentAccountMessage
+    {
+        $this->waitDate = $waitDate;
+        return $this;
+    }
 }
