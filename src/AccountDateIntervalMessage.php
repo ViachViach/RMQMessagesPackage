56813025e6ec28804d7fc3796b4a6c2add80cc43
Karmazin <vyacheslavkarmazin@gmail.com>
2021-08-22 01:19:53 +0200

    - add new messages

--- src/AccountDateIntervalMessage.php
+++ src/AccountDateIntervalMessage.php
@@ -10,4 +10,26 @@ class AccountDateIntervalMessage
 {
     private DateTimeImmutable $dateForm;
     private DateTimeImmutable $dateTo;
+
+    public function getDateForm(): DateTimeImmutable
+    {
+        return $this->dateForm;
+    }
+
+    public function setDateForm(DateTimeImmutable $dateForm): AccountDateIntervalMessage
+    {
+        $this->dateForm = $dateForm;
+        return $this;
+    }
+
+    public function getDateTo(): DateTimeImmutable
+    {
+        return $this->dateTo;
+    }
+
+    public function setDateTo(DateTimeImmutable $dateTo): AccountDateIntervalMessage
+    {
+        $this->dateTo = $dateTo;
+        return $this;
+    }
 }
