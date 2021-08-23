6a871856dc45b397cbcb7818dd50bf6ba34cd336
Karmazin <vyacheslavkarmazin@gmail.com>
2021-08-23 20:52:23 +0200

    - add new messages

--- src/AppointmentAccountMessage.php
+++ src/AppointmentAccountMessage.php
@@ -4,14 +4,74 @@ declare(strict_types=1);
 
 namespace RMQ\Messages;
 
-class AppointmentAccountMessage extends AccountMessage
+class AppointmentAccountMessage
 {
+    private string $login;
+    private string $password;
+    private int $delay;
+    private string $city;
+    private string $visaType;
     /**
      * @var AccountDateIntervalMessage[]
     */
     private array $dateInterval;
     private int $waitDate;
 
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): AppointmentAccountMessage
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): AppointmentAccountMessage
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getDelay(): int
+    {
+        return $this->delay;
+    }
+
+    public function setDelay(int $delay): AppointmentAccountMessage
+    {
+        $this->delay = $delay;
+        return $this;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): AppointmentAccountMessage
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getVisaType(): string
+    {
+        return $this->visaType;
+    }
+
+    public function setVisaType(string $visaType): AppointmentAccountMessage
+    {
+        $this->visaType = $visaType;
+        return $this;
+    }
+    
     /**
      * @return AccountDateIntervalMessage[]
      */
